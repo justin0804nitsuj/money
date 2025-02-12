9b9db211a162f5e634407ce9d01b822e74fbaca0
justin假斯叮 <justinjuju0804@gmail.com>
2025-02-12 23:26:16 +0800

    更新歡迎訊息

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 function login() {
     const username = document.getElementById('username').value;
     if (username) {
-        // 更新使用者的登入次數
-        let loginCount = localStorage.getItem(`${username}_loginCount`) || 0;
-        loginCount++;
-        localStorage.setItem(`${username}_loginCount`, loginCount);
-
-        // 更新總登入人次
-        let totalvisited = localStorage.getItem('totalvisited') || 0;
-        totalvisited++;
-        localStorage.setItem('totalvisited', totalvisited);
-
         // 導向到 main.html 並傳遞使用者名稱
         window.location.href = `main.html?username=${encodeURIComponent(username)}`;
     } else {
@@ -27,11 +17,7 @@ function displayWelcomeMessage() {
     const username = getParameterByName('username');
     if (username) {
         const loginCount = localStorage.getItem(`${username}_loginCount`) || 0;
-        document.getElementById('welcome-message').textContent = `已成功登入 名稱為 ${username}`;
-        document.getElementById('login-count').textContent = `這是您第 ${loginCount} 次登入`;
-
-        const totalvisited = localStorage.getItem('totalvisited') || 0;
-        document.getElementById('total-visited').textContent = `總來訪人次：${totalvisited}`;
+        document.getElementById('welcome-message').textContent = `你好 ${username} !`;
     } else {
         document.getElementById('welcome-message').textContent = `未提供姓名`;
     }
